1e71d5b5cf3bff80252173bb6d792baa8da456ff
repl.it user <replituser@example.com>
2020-12-23 15:04:08 +0000

    co

---
+++ hanoi.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int hanoi(int amount, int start, int end);
+
+int main() {
+	int k;
+	scanf("%d", &k);
+	printf("%d\n", (2<<(k-1))-1);
+	hanoi(k, 1, 3);
+}
+
+int hanoi(int amount, int start, int end) {
+	if (amount <= 1) {
+		printf("%d %d\n", start, end);
+		return 0;
+	}
+	
+	hanoi(amount - 1, start, 6-(start+end));
+	
+	printf("%d %d\n", start, end);
+	
+	hanoi(amount - 1, 6 - (start+end), end);
+}
+
+
+//i cant understand it now fuck
