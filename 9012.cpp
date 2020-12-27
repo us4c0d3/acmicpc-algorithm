3171ec4b2010c4aabd37ed568b716ac6ab7135ef
repl.it user <replituser@example.com>
2020-12-27 15:07:24 +0000

    9012

---
+++ 9012.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#define SIZE 60
+#define STACK_SIZE 30
+#define TRUE 1
+#define FALSE 0
+
+//push, pop, isEmpty
+
+typedef struct _stack{
+  char arr[STACK_SIZE];
+  int top;
+} Stack;
+
+void stack_init(Stack * sp) {
+  sp->top = -1;
+}
+
+bool isEmpty(Stack * sp) {
+  if(sp->top == -1) return TRUE;
+  return FALSE;
+}
+
+void Push(Stack * sp, char data) {
+  sp->arr[++(sp->top)] = data;
+}
+
+
+char Pop(Stack * sp) {
+  if(isEmpty) return 'c';
+  return sp->arr[(sp->top)--];
+}
+
+int main() {
+  int n;
+  Stack vps;
+  scanf("%d", &n);
+
+  for(int i = 0; i < n; i++) {
+    int j = 0, k = 0;
+    char ps[SIZE], temp;
+    scanf("%s", ps);
+    getchar();
+
+    stack_init(&vps);
+
+    while(ps[j] != '\0') {
+      if(ps[j] == '(')  {
+        Push(&vps);
+      }
+      else {
+        temp = Pop(&vps);
+        if(temp != '(') {
+          k = -1;
+          break;
+        }
+      }
+    }
+
+    if(!isEmpty || k == -1) {
+      printf("NO\n");
+      break;
+    }
+    printf("YES\n");
+  }
+
+  return 0;
+}
