bf152a15fc3c676551be88d430e5a5262e885822
repl.it user <replituser@example.com>
2020-12-28 14:40:34 +0000

    commit

---
+++ 5568.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int ans[31][31];
+int main(){
+	int n;
+	scanf("%d", &n);
+	
+	for(int i = 0; i < 31; i++) {
+		ans[i][i] = 1;
+		ans[i][0] = 1;
+		for(int j = 1; j < i; j++) {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+		}
+	}
+
+	for(int k = 0; k < n; k++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d\n", ans[a][b]);
+	}
+
+	return 0;
+}
