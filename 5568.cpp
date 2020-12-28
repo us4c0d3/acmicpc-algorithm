983616d040c0d632994ef4a718d232368deb8160
repl.it user <replituser@example.com>
2020-12-28 14:48:20 +0000

    5568

--- 5568.cpp
+++ 5568.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 int ans[31][31];
+
 int main(){
 	int n;
 	scanf("%d", &n);
@@ -7,7 +9,7 @@ int main(){
 	for(int i = 0; i < 31; i++) {
 		ans[i][i] = 1;
 		ans[i][0] = 1;
-		for(int j = 1; j < i; j++) {
+		for(int j = 1; j <= i-1; j++) {
 			ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
 		}
 	}
@@ -15,7 +17,7 @@ int main(){
 	for(int k = 0; k < n; k++) {
 		int a, b;
 		scanf("%d %d", &a, &b);
-		printf("%d\n", ans[a][b]);
+		printf("%d\n", ans[b][a]);
 	}
 
 	return 0;
