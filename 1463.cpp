1ceab320f9e68fe8569bfb143da129ce10ef8ee2
repl.it user <replituser@example.com>
2021-01-02 14:59:38 +0000

    get 2 problems

---
+++ 1463.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int min(int x, int y) {return x<y?x:y;}
+int ans[1000001] = {0, 0};
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	for(int i = 2; i <= n; i++) {
+		ans[i] = ans[i-1] + 1; //rule 3
+		if(i % 3 == 0) {
+			ans[i] = min(ans[i], ans[i/3] + 1); //rule 1
+		}
+		if (i % 2 == 0) {
+			ans[i] = min(ans[i], ans[i/2] + 1); //rule 2
+		}
+	}
+
+	printf("%d\n", ans[n]);
+
+	return 0;
+}
+
