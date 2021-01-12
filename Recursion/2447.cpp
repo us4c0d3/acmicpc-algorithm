867390a3eb9ba0bf309e5bba0a54ba7e8d66d09d
repl.it user <replituser@example.com>
2021-01-12 14:22:34 +0000

    link

---
+++ Recursion/2447.cpp
@@ -0,0 +1,26 @@
+//noj.am/2447
+//Recursion
+#include <stdio.h>
+
+void draw(int x, int y, int n) {
+  if((x/n) % 3 == 1 && (y/n) % 3 == 1) {
+    printf(" ");
+  } else {
+    if(n / 3 == 0) {
+      printf("*");
+    } else {
+      draw(x, y, n/3);
+    }
+  }
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      draw(i, j, n);
+    }
+    printf("\n");
+  }
+}
