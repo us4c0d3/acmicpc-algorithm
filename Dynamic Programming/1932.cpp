cf0f75e539d29474b64c053db8efe6d1d2d5afe7
akamono312 <jwseok312@gmail.com>
2023-01-24 12:43:25 +0000

    20230124

--- Dynamic Programming/1932.cpp
+++ Dynamic Programming/1932.cpp
@@ -1,35 +1,32 @@
-//https://www.acmicpc.net/problem/1932
-//Dynamic Programming
-#include <stdio.h>
+#include <iostream>
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl '\n'
+using namespace std;
 
-int max(int x, int y) {return x>y?x:y;}
+int max(int a, int b) {return a>b?a:b;}
 
+int tri[500][500];
 int main() {
-	int n, i, j;
-  int sum = 0;
-	int tri[500][500] = {0, };
-  scanf("%d", &n);
+    fastio;
+    int n, i, j;
+    int maxSum = 0;
+    cin >> n;
 
-	for(i = 0; i < n; i++) {
-		for(j = 0; j <= i; j++) {
-			scanf("%d", &tri[i][j]);
-		}
-	}
-
-  for(i = 1; i < n; i++) {
-    for(j = 0; j <= i; j++) {
-      if(j == 0) {
-        tri[i][j] += tri[i-1][0];
-      } else if(i == j) {
-        tri[i][j] += tri[i-1][j-1];
-      } else {
-        tri[i][j] += max(tri[i-1][j-1], tri[i-1][j]);
-      }
-      sum = max(sum, tri[i][j]);
+    for(i = 0; i < n; i++) {
+        for(j = 0; j <= i; j++) {
+            cin >> tri[i][j];
+        }
     }
-  }
 
-  printf("%d", sum);
+    for(i = 0; i < n; i++) {
+        for(j = 0; j <= i; j++) {
+            if(j == 0) tri[i][j] += tri[i - 1][0];
+            else if(i == j) tri[i][j] += tri[i - 1][j - 1];
+            else tri[i][j] += max(tri[i - 1][j], tri[i - 1][j - 1]);
 
-  return 0;
+            maxSum = max(maxSum, tri[i][j]);
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
 }
