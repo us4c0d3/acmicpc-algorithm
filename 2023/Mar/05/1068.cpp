50cdfcdb8da0d8b088ec575183250b1a9780ff6b
akamono312 <jwseok312@gmail.com>
2023-03-05 08:25:00 +0000

    20230305

--- 2023/Mar/05/1068.cpp
+++ 2023/Mar/05/1068.cpp
@@ -8,19 +8,18 @@ using u = unsigned;
 using pii = pair<int, int>;
 
 int n, cnt, d;
-int root = -1;
+int root;
 vector<int> v[51];
-bool visited[51];
 
 void dfs(int cur) {
-    if(visited[cur]) return;
-
-    visited[cur] = true;
+    if(d == cur) return;
+    
     if(v[cur].size() == 0 || (v[cur].size() == 1 && v[cur][0] == d)) cnt++;
-
-    for(int i = 0; i < v[cur].size(); i++) {
-        if(v[cur][i] != d) {
-            dfs(v[cur][i]);
+    else {
+        for(int i = 0; i < v[cur].size(); i++) {
+            if(v[cur][i] != d) {
+                dfs(v[cur][i]);
+            }
         }
     }
 }
@@ -29,17 +28,14 @@ int main() {
     fastio;
     cin >> n;
     for(int i = 0; i < n; i++) {
-        int t;
-        cin >> t;
-        if(t == -1) root = i;
-        else v[t].push_back(i);
+        int parent;
+        cin >> parent;
+        if(parent == -1) root = i;
+        else v[parent].push_back(i);
     }
 
     cin >> d;
-    if(d == root) cout << 0 << endl;
-    else {
-        dfs(root);
-    }
+    dfs(root);
     cout << cnt << endl;
     
     return 0;
