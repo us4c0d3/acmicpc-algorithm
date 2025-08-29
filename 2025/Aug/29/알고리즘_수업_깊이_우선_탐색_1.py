9d047112cdecf14a8df5265e259c6e1f960f70e4
us4c0d3 <us4c0d3@gmail.com>
2025-08-30 02:00:49 +0900

    DFS

---
+++ 2025/Aug/29/알고리즘_수업_깊이_우선_탐색_1.py
@@ -0,0 +1,33 @@
+from sys import stdin as s
+import sys
+sys.setrecursionlimit(2000001)
+input = lambda: s.readline().rstrip()
+
+# main code
+def dfs(v, e, r):
+    global visited
+    global res
+    global cnt
+    cnt += 1
+    visited[r] = True
+    res[r - 1] = cnt
+    for x in e[r]:
+        if not visited[x]:
+            dfs(v, e, x)
+
+n, m, r = map(int, input().split())
+v = [i for i in range(1, n + 1)]
+e = [[] for i in range(n + 1)]
+visited = [False] * (n + 1)
+res = [0] * n
+cnt = 0
+for _ in range(m):
+    a, b = map(int, input().split())
+    e[a].append(b)
+    e[b].append(a)
+
+for i in range(1, n + 1):
+    e[i].sort()
+
+dfs(v, e, r)
+print(*res, sep='\n')
