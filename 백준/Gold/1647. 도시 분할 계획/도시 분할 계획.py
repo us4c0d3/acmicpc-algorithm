2014581735cdbeac34bf1122206cf0c45405c674
Wooseok Jang <us4c0d3@gmail.com>
2026-02-27 22:00:04 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3368 ms, Memory: 279276 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,40 +1,68 @@
 from sys import stdin
 input = lambda: stdin.readline().rstrip()
 
-# main code
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+# Kruskal
+# def find(x):
+#     if parent[x] != x:
+#         parent[x] = find(parent[x])
+#     return parent[x]
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a != b:
-        if rank[a] > rank[b]:
-            parent[b] = a
-        elif rank[a] < rank[b]:
-            parent[a] = b
-        else:
-            parent[b] = a
-            rank[a] += 1
+# def union(a, b):
+#     a = find(a)
+#     b = find(b)
+#     if a != b:
+#         if rank[a] > rank[b]:
+#             parent[b] = a
+#         elif rank[a] < rank[b]:
+#             parent[a] = b
+#         else:
+#             parent[b] = a
+#             rank[a] += 1
 
 
 n, m = map(int, input().split())
-edges = []
-parent = [i for i in range(n + 1)]
-rank = [0] * (n + 1)
+# edges = []
+# parent = [i for i in range(n + 1)]
+# rank = [0] * (n + 1)
+# for _ in range(m):
+#     a, b, c = map(int, input().split())
+#     edges.append((c, a, b))
+
+# edges.sort()
+# res = 0
+# max_edge = 0
+# for c, a, b in edges:
+#     if find(a) != find(b):
+#         union(a, b)
+#         res += c
+#         max_edge = c
+
+# print(res - max_edge)
+
+# Prim
+import heapq
+
+pq = []
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b, c = map(int, input().split())
-    edges.append((c, a, b))
+    graph[a].append((b, c))
+    graph[b].append((a, c))
 
-edges.sort()
-res = 0
+visited = [False] * (n + 1)
+heapq.heappush(pq, (0, 1))
 max_edge = 0
-for c, a, b in edges:
-    if find(a) != find(b):
-        union(a, b)
-        res += c
-        max_edge = c
+res = 0
+while pq:
+    cost, cur = heapq.heappop(pq)
+    if visited[cur]:
+        continue
+    visited[cur] = True
+    res += cost
+    max_edge = max(max_edge, cost)
+
+    for nxt, nxt_cost in graph[cur]:
+        if not visited[nxt]:
+            heapq.heappush(pq, (nxt_cost, nxt))
 
 print(res - max_edge)
