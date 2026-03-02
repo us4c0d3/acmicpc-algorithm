4c2c0f3da2bc96f3da4083c61b427dcb6fd6dcba
Wooseok Jang <us4c0d3@gmail.com>
2026-03-02 23:58:58 +0900

    [Gold IV] Title: LCS, Time: 116 ms, Memory: 118564 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,20 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+# main code
+s1 = input()
+s2 = input()
+n = len(s1)
+m = len(s2)
+
+dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        if s1[i - 1] == s2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[n][m])
